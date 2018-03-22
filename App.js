ebd91fd2441c152a631a665d1ca9f6eec9c12cad
KEN-chan <hellokenta1024@gmail.com>
2018-03-21 22:20:32 -0400

    Go to detail screen

--- App.js
+++ App.js
@@ -5,7 +5,7 @@
  */
 
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
 class HomeScreen extends React.Component {
@@ -13,16 +13,39 @@ class HomeScreen extends React.Component {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Home Screen</Text>
+        <Button
+          title = "Go to Detail"
+          onPress = { () => this.props.navigation.navigate('Details') }
+        />
       </View>
     );
   }
 }
 
-const RootStack = StackNavigator({
-  Home: {
-    screen: HomeScreen,
+class DetailsScreen extends React.Component {
+
+  render() {
+    return (
+      <View style = {{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
+}
+
+const RootStack = StackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    Details: {
+      screen: DetailsScreen,
+    },
   },
-});
+  {
+    initialRouteName: 'Home',
+  }
+);
 
 export default class App extends React.Component {
 
